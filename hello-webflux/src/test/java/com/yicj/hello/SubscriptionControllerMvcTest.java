60248b1b86469dbe68571305e49fb2a79e04ec3c
易成杰 <626659321@qq.com>
2023-04-23 19:02:22 +0800

    save

--- hello-webflux/src/test/java/com/yicj/hello/SubscriptionControllerMvcTest.java
+++ hello-webflux/src/test/java/com/yicj/hello/SubscriptionControllerMvcTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -34,6 +35,7 @@ public class SubscriptionControllerMvcTest {
 
 
     @Test
+    @WithMockUser(username = "test@qq.com", roles = "ADMIN")
     public void shouldReturnViewSubs() throws Exception {
         when(subscriptionService.findByEmail(anyString())).thenReturn(buildSubscriptionList()) ;
         this.mockMvc.perform(MockMvcRequestBuilders.get("/subscriptions"))
